e518af0a69ea3606df0c95d215ebe451700b197c
jackwwt <51823914+jackwwt@users.noreply.github.com>
2023-12-17 20:28:05 +0800

    Delete models directory

--- models/__init__.py
+++
@@ -1,4 +0,0 @@
-#from .vision_transformer import VisionTransformer, vit_tiny, vit_small, vit_base, vit_large
-#from .swin_transformer import SwinTransformer, swin_tiny, swin_small, swin_base, swin_large
-from .mae import MaskedAutoencoderViT, mae_vit_base_patch16, mae_vit_large_patch16, mae_vit_huge_patch14
-    
