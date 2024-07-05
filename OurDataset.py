0f03ae859932c94c7e467689d2b4b79d59fc47b1
jackwwt <51823914+jackwwt@users.noreply.github.com>
2024-07-05 17:42:31 +0800

    Update OurDataset.py

--- OurDataset.py
+++ OurDataset.py
@@ -6,15 +6,15 @@ import torch
 from typing import Tuple, Optional, Union
 
 class OurDataset(Dataset):
-    def __init__(self, pkl_path,seed, transform = None):
+    def __init__(self, pkl_path, transform = None):
         dataset_info = pickle.load(open(pkl_path, 'rb+'))
 
-        self.seed = seed
         self.transform=transform
         self.rgb_image_name = dataset_info.image_rgb_name
         self.lunkuo_image_name = dataset_info.image_lunkuo_name
     def __getitem__(self, index)-> Tuple[torch.Tensor, ...]:
 
+        seed = torch.randint(0, 100000, (1,)).item()
         rgb_image= self.rgb_image_name[index]
         lunkuo_image= self.lunkuo_image_name[index]
 
@@ -22,9 +22,9 @@ class OurDataset(Dataset):
         lunkuo_img_pil = Image.open(lunkuo_image).convert('RGB')
 
         if self.transform is not None:
-            torch.manual_seed(self.seed)
+            torch.manual_seed(seed)
             rgb_image = self.transform(rgb_img_pil)
-            torch.manual_seed(self.seed)
+            torch.manual_seed(seed)
             lunkuo_image = self.transform(lunkuo_img_pil)
 
         return rgb_image,lunkuo_image
